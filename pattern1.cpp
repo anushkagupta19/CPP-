09dd7bb1320e616bbeb650299e4e1b1428329009
Anushka <anushkamor19@gmail.com>
2025-11-17 22:45:42 +0530

    2

---
+++ pattern1.cpp
@@ -0,0 +1,19 @@
+// 1 1 1 1
+// 2 2 2 2
+// 3 3 3 3
+// 4 4 4 4
+
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+cout<<"Enter number of rows: ";
+cin>>n;
+for(int i=1;i<=n;i++){
+int val=i;
+    for(int j=1;j<=n;j++){
+        cout<<val<<" ";
+    }
+      cout<<endl;
+    }
+}
