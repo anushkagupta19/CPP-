318a04820249beaea41c33fbe9808f733ef2e73a
Anushka <anushkamor19@gmail.com>
2025-11-18 21:56:14 +0530

    3

---
+++ pattern7.cpp
@@ -0,0 +1,25 @@
+//           *
+//         * *
+//       * * * 
+//     * * * * 
+//   * * * * *
+// * * * * * *
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        // Print spaces
+        for(int j=1;j<=n-i;j++){
+            cout<<"  ";
+        }
+        // Print stars
+        for(int k=1;k<=i;k++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
