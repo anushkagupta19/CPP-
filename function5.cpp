3eae37e9afca84120e4d8a466a0c27bfa2057f06
Anushka <anushkamor19@gmail.com>
2025-11-22 21:57:42 +0530

    todays work

---
+++ function5.cpp
@@ -0,0 +1,16 @@
+// WAP to print if a number is prime or not.
+
+#include<iostream>
+using namespace std;
+bool iseven(int n){
+if(n%2==0){
+   return true;
+}else{
+    return false;
+   }
+}
+
+int main(){
+    cout<<iseven(6)<<endl;
+    return 0;
+}
