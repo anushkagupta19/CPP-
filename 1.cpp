c6dc4afc6f1b761af828b399bffd7e8bd0a1929f
Anushka <anushkamor19@gmail.com>
2025-11-25 12:20:51 +0530

    todays work

---
+++ 1.cpp
@@ -0,0 +1,19 @@
+// Accept the length and width of a rectangle. 
+// Calculate & print the area and perimiter.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    float length,width;
+    cout<<"Enter length of rectangle: ";
+    cin>>length;
+    cout<<"Enter width of rectangle: ";
+    cin>>width;
+
+    float area=length*width;
+    float perimeter=2*(length+width);
+
+    cout<<"Area: "<<area<<endl;
+    cout<<"Perimeter: "<<perimeter<<endl;
+}
---
+++ 2.cpp
@@ -0,0 +1,22 @@
+//  Accept the parameters and calculate the 
+// Compound Interest & print it on STDOUT 
+// (Use Math class methods).
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+    float principal,rate,time,CI;
+    cout<<"Enter principal amount: ";
+    cin>>principal;
+    cout<<"Enter rate of interest: ";
+    cin>>rate;
+    cout<<"Enter time in years: ";
+    cin>>time;
+
+    CI = principal * (pow((1 + rate / 100), time)) - principal;
+    cout<<"Compound Interest is: "<<CI<<endl;
+
+    return 0;
+}
