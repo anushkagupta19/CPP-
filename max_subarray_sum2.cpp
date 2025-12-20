9695e8a5bf4842ca67794c2892af250bb12ad69c
Anushka <anushkamor19@gmail.com>
2025-12-21 01:39:06 +0530

    work

--- max_subarray_sum2.cpp
+++ max_subarray_sum2.cpp
@@ -1,31 +1,28 @@
-// time complexity=O(n^3)
+// time complexity=O(n^2)
 
 #include<iostream>
 #include<climits>
 #include<algorithm>
-using namespace std;
 
-void maxSubarraySum1(int *arr, int n) {
+using namespace std;
+void maxSubarraySum2(int *arr, int n) {
     int maxsum = INT_MIN;
 
     for (int start = 0; start < n; start++) { // starting point of subarray
+        int currentSum = 0;
         for(int end = start; end < n; end++){// ending point of subarray
-            int currentSum = 0;
-            for(int i = start; i <= end; i++){// to calculate sum of subarray
-                currentSum += arr[i];
-            }
-            cout << currentSum << ",";
+            currentSum += arr[end]; // incrementally add to current sum
+            // cout << currentSum << ",";
             maxsum = max(maxsum, currentSum);// updating maximum sum
         }
-    cout << endl;
+        // cout << endl;
     }
     cout << "The maximum subarray sum is: " << maxsum << endl;
 }
-
 int main() {
     int arr[9]={-2,1,-3,4,-1,2,1,-5,4};
     int n = sizeof(arr) / sizeof(int);
     
-    maxSubarraySum1(arr, n);
+    maxSubarraySum2(arr, n);
     return 0;
 }
