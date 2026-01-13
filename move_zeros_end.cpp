1345af88d8ba95d92b3c78fb5358fe0b6ccc2b81
Anushka <anushkamor19@gmail.com>
2026-01-13 14:33:15 +0530

    work

--- move_zeros_end.cpp
+++ move_zeros_end.cpp
@@ -2,23 +2,22 @@
 using namespace std;
 
 void moveZeroes(vector<int>& nums) {
-    int left = 0;
     
+    int left = 0;
     for(int right = 0; right < nums.size(); right++){
         if(nums[right] != 0) {
-            swap(nums[left], nums[right]);
+            swap(nums[left],nums[right]);
             left++;
         }
     }
 }
 
 int main() {
-    vector<int> nums = {0, 1, 0, 3, 12};
+    vector<int> nums = {0, 1, 0, 3, 0,9,0 , 0,12};
     moveZeroes(nums);
     
     for(int num : nums) {
         cout << num << " ";
-    }
-    
+    } 
     return 0;
 }
