0e6456b5cef1f7e1be3c07e41789b8410ee60b1f
Anushka <anushkamor19@gmail.com>
2026-01-22 22:33:55 +0530

    work

--- pattern1.cpp
+++ pattern1.cpp
@@ -5,7 +5,9 @@
 
 #include<iostream>
 using namespace std;
+
 int main(){
+    
 int n;
 cout<<"Enter number of rows: ";
 cin>>n;
--- pattern6.cpp
+++ pattern6.cpp
@@ -7,8 +7,7 @@ using namespace std;
 
 int main(){
     
-    int n;
-    cin>>n;
+    int n=8;
     for(int i=1;i<=n;i++){
         cout<<"*";
         for(int j=1;j<=n-1;j++){
