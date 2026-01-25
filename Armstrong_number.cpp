3cfd68ff181ccb343098a21d8ed8937c94356c82
Anushka <anushkamor19@gmail.com>
2026-01-25 20:34:22 +0530

    work

--- Armstrong_number.cpp
+++ Armstrong_number.cpp
@@ -10,15 +10,22 @@ using namespace std;
 int main(){
 
     int num;
+
     int originalNum;
+
     int remainder; 
+
     int result = 0;
+
     cout << "Enter a 3-digit number: ";
     cin >> num;
+
     originalNum = num;
 
     while (originalNum != 0) {
+
         remainder = originalNum % 10;
+        
         result += remainder * remainder * remainder;
         originalNum /= 10;
     }
