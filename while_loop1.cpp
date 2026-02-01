7d67cb012978c176cb0fcf25aa68b2c8fa25e15d
Anushka <anushkamor19@gmail.com>
2026-02-01 06:44:03 +0530

    work

---
+++ while_loop1.cpp
@@ -0,0 +1,13 @@
+// 1. Print all numbers from 1 to 10 using a loop.
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 1;
+    while (i <= 10) {
+        cout << i << " ";
+        i++;
+    }
+    return 0;
+}
+
