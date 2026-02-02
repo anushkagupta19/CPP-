0f484fc071896e2076b20bd476e7f6338e30c1a8
Anushka <anushkamor19@gmail.com>
2026-02-02 06:08:54 +0530

    work

---
+++ while_loop2.cpp
@@ -0,0 +1,12 @@
+//2. Print numbers from 10 down to 1 in reverse order.
+
+#include <iostream>
+using namespace std;
+int main() {
+    int i = 10;
+    while (i >= 1) {
+        cout << i << " "<<endl;
+        i--;
+    }
+    return 0;
+}
