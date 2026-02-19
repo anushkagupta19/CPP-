389c9adb78b4c6665fc9f5f9c554e264dbb6effa
Anushka <anushkamor19@gmail.com>
2026-02-19 20:44:10 +0530

    work

--- binTodec.cpp
+++ binTodec.cpp
@@ -2,17 +2,20 @@
 using namespace std;
 
 void binTodec(int binNum){
+
     int n=binNum;
     int decNum=0;
     int pow=1;
 
     while(n>0){
+        
         int lastdigit= n%10;
         decNum+=lastdigit*pow;
         pow=pow*2;
         n=n/10;
     }
-cout<<decNum<<endl;
+
+      cout<<decNum<<endl;
 }
 
 int main(){
