eb26615eba8a970e8fac6f3e503beefeed53444a
Anushka <anushkamor19@gmail.com>
2026-02-20 22:14:06 +0530

    wor

--- duplicate_num.cpp
+++ duplicate_num.cpp
@@ -6,17 +6,21 @@ using namespace std;
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
+
         // We use an unordered_set to store unique elements we've seen so far
         unordered_set<int> seen;
         
         for (int x : nums) {
+
             // If the element is already in the set, we found a duplicate
             if (seen.find(x) != seen.end()) {
                 return true;
             }
+
             // Otherwise, add the element to the set
             seen.insert(x);
         }
+
         // If we finish the loop, all elements are distinct
         return false;
     }
@@ -27,8 +31,10 @@ int main() {
     vector<int> nums = {1, 2, 3, 1};
     
     if (solution.containsDuplicate(nums)) {
+
         cout << "Contains duplicate" << endl;
     } else {
+        
         cout << "No duplicates" << endl;
     }
     
