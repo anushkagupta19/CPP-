b1fe3eba9a94db692e73c2f1cbb1bb98e432aa68
Anushka <anushkamor19@gmail.com>
2026-03-24 23:23:01 +0530

    work

---
+++ 8.cpp
@@ -0,0 +1,123 @@
+// 21. Print the square of each number from 1 to n.
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int square =1;
+//     for(int i=1;i<=100;++i){
+//         square=i*i;
+//         cout<<square<<" ";
+//     }
+//     return 0;
+// }
+// 22. Print the cube of each number from 1 to n.
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int cube=1;
+//     for(int i=1;i<100;++i){
+//         cube=i*i*i;
+//         cout<<cube<<" ";
+//     }
+// }
+// 23. Print all numbers between a and b that are divisible by 7.
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int b=20;
+//     for(int a=1;a<=b;++a){
+//           if(a%7==0){
+//             cout<<a<<endl;
+//           }
+//     }
+//     return 0;
+// }
+// 24. Print all factors of the given number.
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n=24;
+//     int i=1;
+//     while(i<=n){
+//         if(n%i==0){
+//             cout<<i<<" ";
+         
+//         }
+//         i++;
+//     }
+//     return 0;
+// }
+
+// 25. Find and print the sum of all factors of the given number.
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int sum=0;
+//     int i=1;
+//     int n=24;
+//     cout<<" the sum is :";
+//     while(i<=n){
+//         if(n%i==0){
+//             sum=sum+i;
+//         }
+//         i++;
+//     }
+//     cout<<sum<<" ";
+//     return 0;
+// }
+
+// 26. Find the HCF (Highest Common Factor) of two given numbers.
+
+// 27. Find the LCM (Least Common Multiple) of two given numbers.
+// 28. Find the smallest digit in the given number.
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int n, smallest = 9; // Initialize with the largest possible digit
+//     cout << "Enter a positive integer: ";
+//     cin >> n;
+//     // Handle the case if the user enters 0
+//     if (n == 0) {
+//         smallest = 0;
+//     }
+//     // Make n positive if the user enters a negative number
+//     if (n < 0) n = -n;
+
+//     while (n > 0) {
+//         int digit = n % 10; // Extract the last digit
+//         if (digit < smallest) {
+//             smallest = digit; // Update smallest if we find a lower number
+//         }
+//         n /= 10; // Remove the last digit
+//     }
+
+//     cout << "The smallest digit is: " << smallest << endl;
+//     return 0;
+// }
+
+// 29. Find the largest digit in the given number.
+#include <iostream>
+using namespace std;
+int main() {
+    int n, largest = 0; // Initialize with the smallest possible digit
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    // Handle negative numbers by making them positive
+    if (n < 0) n = -n;
+
+    // Special case for 0
+    if (n == 0) {
+        largest = 0;
+    }
+
+    while (n > 0) {
+        int digit = n % 10; // Get the last digit
+        if (digit > largest) {
+            largest = digit; // Update if current digit is bigger
+        }
+        n /= 10; // Remove the last digit
+    }
+    cout << "The largest digit is: " << largest << endl;
+    return 0;
+}
