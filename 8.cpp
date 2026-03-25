2500f0c74e100e9ecab73d665042ddfc47f6ef20
Anushka <anushkamor19@gmail.com>
2026-03-25 23:44:34 +0530

    work

--- 8.cpp
+++ 8.cpp
@@ -1,101 +1,151 @@
 // 21. Print the square of each number from 1 to n.
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     int square =1;
-//     for(int i=1;i<=100;++i){
-//         square=i*i;
-//         cout<<square<<" ";
-//     }
-//     return 0;
-// }
+#include<iostream>
+using namespace std;
+int main(){
+    int square =1;
+    for(int i=1;i<=100;++i){
+        square=i*i;
+        cout<<square<<" ";
+    }
+    return 0;
+}
 // 22. Print the cube of each number from 1 to n.
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     int cube=1;
-//     for(int i=1;i<100;++i){
-//         cube=i*i*i;
-//         cout<<cube<<" ";
-//     }
-// }
+#include<iostream>
+using namespace std;
+int main(){
+    int cube=1;
+    for(int i=1;i<100;++i){
+        cube=i*i*i;
+        cout<<cube<<" ";
+    }
+}
 // 23. Print all numbers between a and b that are divisible by 7.
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     int b=20;
-//     for(int a=1;a<=b;++a){
-//           if(a%7==0){
-//             cout<<a<<endl;
-//           }
-//     }
-//     return 0;
-// }
+#include<iostream>
+using namespace std;
+int main(){
+    int b=20;
+    for(int a=1;a<=b;++a){
+          if(a%7==0){
+            cout<<a<<endl;
+          }
+    }
+    return 0;
+}
 // 24. Print all factors of the given number.
 
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     int n=24;
-//     int i=1;
-//     while(i<=n){
-//         if(n%i==0){
-//             cout<<i<<" ";
+#include<iostream>
+using namespace std;
+int main(){
+    int n=24;
+    int i=1;
+    while(i<=n){
+        if(n%i==0){
+            cout<<i<<" ";
          
-//         }
-//         i++;
-//     }
-//     return 0;
-// }
+        }
+        i++;
+    }
+    return 0;
+}
 
 // 25. Find and print the sum of all factors of the given number.
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     int sum=0;
-//     int i=1;
-//     int n=24;
-//     cout<<" the sum is :";
-//     while(i<=n){
-//         if(n%i==0){
-//             sum=sum+i;
-//         }
-//         i++;
-//     }
-//     cout<<sum<<" ";
-//     return 0;
-// }
+#include<iostream>
+using namespace std;
+int main(){
+    int sum=0;
+    int i=1;
+    int n=24;
+    cout<<" the sum is :";
+    while(i<=n){
+        if(n%i==0){
+            sum=sum+i;
+        }
+        i++;
+    }
+    cout<<sum<<" ";
+    return 0;
+}
 
 // 26. Find the HCF (Highest Common Factor) of two given numbers.
 
+#include <iostream>
+using namespace std;
+int main() {
+    int n1, n2;
+    cout << "Enter two positive integers: ";
+    cin >> n1 >> n2;
+
+    // Store original values for the final output
+    int a = n1;
+    int b = n2;
+
+    while (b != 0) {
+        int remainder = a % b;
+        a = b;         // Move b to a
+        b = remainder; // Move remainder to b
+    }
+    cout << "The HCF of " << n1 << " and " << n2 << " is: " << a <<endl;
+    return 0;
+}
 // 27. Find the LCM (Least Common Multiple) of two given numbers.
+
+#include <iostream>
+using namespace std;
+int main() {
+    long long n1, n2, a, b;
+    cout << "Enter two positive integers: ";
+    cin >> n1 >> n2;
+
+    // We store the original values to use in the formula later
+    a = n1;
+    b = n2;
+
+    // 1. Find the HCF using the Euclidean Algorithm
+    while (b != 0) {
+        long long remainder = a % b;
+        a = b;
+        b = remainder;
+    }
+    long long hcf = a;
+    // 2. Use the formula: LCM = (n1 * n2) / HCF
+    // We multiply first, then divide. 
+    long long lcm = (n1 * n2) / hcf;
+
+   cout << "The HCF is: " << hcf << endl;
+   cout << "The LCM is: " << lcm << endl;
+
+    return 0;
+}
+
 // 28. Find the smallest digit in the given number.
-// #include <iostream>
-// using namespace std;
-// int main() {
-//     int n, smallest = 9; // Initialize with the largest possible digit
-//     cout << "Enter a positive integer: ";
-//     cin >> n;
-//     // Handle the case if the user enters 0
-//     if (n == 0) {
-//         smallest = 0;
-//     }
-//     // Make n positive if the user enters a negative number
-//     if (n < 0) n = -n;
 
-//     while (n > 0) {
-//         int digit = n % 10; // Extract the last digit
-//         if (digit < smallest) {
-//             smallest = digit; // Update smallest if we find a lower number
-//         }
-//         n /= 10; // Remove the last digit
-//     }
+#include <iostream>
+using namespace std;
+int main() {
+    int n, smallest = 9; // Initialize with the largest possible digit
+    cout << "Enter a positive integer: ";
+    cin >> n;
+    // Handle the case if the user enters 0
+    if (n == 0) {
+        smallest = 0;
+    }
+    // Make n positive if the user enters a negative number
+    if (n < 0) n = -n;
 
-//     cout << "The smallest digit is: " << smallest << endl;
-//     return 0;
-// }
+    while (n > 0) {
+        int digit = n % 10; // Extract the last digit
+        if (digit < smallest) {
+            smallest = digit; // Update smallest if we find a lower number
+        }
+        n /= 10; // Remove the last digit
+    }
+
+    cout << "The smallest digit is: " << smallest << endl;
+    return 0;
+}
 
 // 29. Find the largest digit in the given number.
+
 #include <iostream>
 using namespace std;
 int main() {
---
+++ 9.cpp
@@ -0,0 +1,12 @@
+Print all numbers from 1 to 10.
+2. Print the multiplication table of a given number.
+3. Keep taking numbers from the user until 0 is entered, then print the sum of all
+entered numbers.
+4. Keep taking numbers from the user until 0 is entered, then print the largest number
+among all inputs.
+5. Count and print the number of digits in the given number.
+6. Reverse the given number and print the reversed value.
+7. Check whether the given number is a palindrome.
+8. Check whether the given number is an Armstrong number.
+9. Calculate and print the factorial of the given number.
+10. Print the Fibonacci series up to the required number of terms.
