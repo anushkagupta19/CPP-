9e203797be17f5ecc618342718e41c2aed65c18d
Anushka <anushkamor19@gmail.com>
2026-03-29 22:42:47 +0530

    work

---
+++ 10.cpp
@@ -0,0 +1,47 @@
+// using for loop:
+
+// Print all even numbers between 1 and 100.
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<" the even numbers are:";
+//     for(int i=0;i<100;i++){
+//         if(i%2==0){
+//             cout<<i<<" ";
+//         }
+//     }
+//     cout<<endl;
+//     return 0;
+// }
+// 4. Print all odd numbers between 1 and 100.
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<" the odd numbers are: ";
+//     for(int i=0;i<100;i++){
+//         if(i%2!=0){
+//             cout<<i<<" ";
+//         }
+//     }
+//     cout<<endl;
+//     return 0;
+// }
+
+// 5. Print the multiplication table of a given number.
+
+// 6. Calculate and print the factorial of a given number.
+// 7. Calculate and print the factorial of every number from 1 to n.
+// 8. Print all prime numbers between 1 and 100.
+// 9. Check whether the given number is a prime number.
+// 10. Print the Fibonacci series up to the required number of terms.
+// 11. Find and print the sum of the Fibonacci series.
+// 12. Print all factors of the given number.
+// 13. Find and print the sum of all factors of the given number.
+// 14. Find the HCF (Highest Common Factor) of the given numbers.
+// 15. Find the LCM (Least Common Multiple) of the given numbers.
+// 16. Print the square of each number from 1 to n.
+// 17. Print the cube of each number from 1 to n.
+// 18. Print all numbers between a and b that are divisible by 7.
+// 19. Find and print the sum of the first n natural numbers.
+// 20. Find and print the sum of all even numbers from 1 up to n.
+// 21. Find and print the sum of all odd numbers from 1 up to n.
